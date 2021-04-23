835b3b2a51e3036579a676020a98d9a7c2cae1f8
nCorneille <n.corneille@student.tue.nl>
2021-04-23 11:57:51 +0200

    Added matrix for the runs rule involving 6 points above or below the median; CCC-chart based test case involving discrete distributions added.

--- src/main.py
+++ src/main.py
@@ -15,8 +15,17 @@ def main():
                                   {"p0": "phi(k)-phi(-k)"},
                                   {"phi": stats.norm.cdf})
 
+    p = 0.05
+    r = 1
+    CCC_model = MarkovChainModel(prev_dir + "matrix_CCC_no-trends",
+                                 # TODO: cdf(k) should be cdf(LCL), with LCL = f(k)
+                                 {"p(X=m)": "pmf(median)", "p(X>m)": "1-cdf(median)", "p(l<X<m)": "cdf(median-1)-cdf(k)"},
+                                 {"median": 3, "pmf": lambda k: stats.nbinom.pmf(k, r, p),
+                                  "cdf": lambda k: stats.nbinom.cdf(k, r, p)})
+
     print("C2 ARL: {}".format(C2_model.calculate_ARL(3)))
     print("no RR ARL: {}".format(test_model.calculate_ARL(3)))
+    print("CCC model no trends: {}".format(CCC_model.calculate_ARL(3)))
 
 
 if __name__ == "__main__":
